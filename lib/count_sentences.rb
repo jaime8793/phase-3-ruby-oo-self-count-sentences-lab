4719e0702017bd5ae937e6603dfd7d97047eb61a
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2022-10-31 14:25:37 +0300

    Completed Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,52 @@ require 'pry'
 class String
 
   def sentence?
+    if self.end_with?(".")
+      true
+      
+    else
+      false
+      
+    end
     
   end
 
   def question?
+    if self.end_with?("?")
+      true
+      
+    else
+      false
+      
+    end
 
   end
 
   def exclamation?
+    if self.end_with?("!")
+      true
+      
+    else
+      false
+      
+    end
+
 
   end
 
   def count_sentences
+   if self.split(/[.?!]/).count > 1
+      self.split(/[.?!]/).count
+   elsif 
+      self.split(/[.?!]/).count == 1
+      self.split(/[.?!]/).count
+    
+   else
+      self.split(/[.?!]/).count
+      
+    
+   end
+
 
   end
 end
